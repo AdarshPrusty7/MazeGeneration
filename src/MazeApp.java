a86c46c8ff8a844c6b142df0707aadc676ed41b7
AdarshPrusty7 <ap910@exeter.ac.uk>
2021-11-02 20:03:24 +0000

    Image Generated
    Implemented JComponent visualisation of maze.

--- src/MazeApp.java
+++ src/MazeApp.java
@@ -1,8 +1,18 @@
+import javax.swing.*;
+
 public class MazeApp {
     public static void main (String[] args) {
-        Maze maze = new Maze(2,2);
+        //Maze maze = new Maze(3,3);
 
+        MazeDrawing mazeDrawing = new MazeDrawing(10, 10);
+        JFrame jf = new JFrame();
 
+        int windowSize = Math.min(mazeDrawing.getWindowHeight(), mazeDrawing.getWindowWidth());
+        jf.setTitle("Maze Generation");
+        jf.setSize(windowSize+150,windowSize+150); // Refers to size of window
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.add(mazeDrawing);
     }
 
 }
---
+++ src/MazeDrawing.java
@@ -0,0 +1,52 @@
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MazeDrawing extends JPanel {
+    private Maze maze;
+    private int windowHeight;
+    private int windowWidth;
+
+    public MazeDrawing(int width, int height) {
+        this.maze = new Maze(width, height);
+        this.windowHeight = 800;
+        this.windowWidth = 1500;
+    }
+
+    public void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(3f));
+
+        int multiplier = Math.min(800/maze.getHeight(), 1500/ maze.getWidth());
+        int adjustor = 50;
+
+        for (int col=0; col<maze.getWidth(); col++) {
+            for (int row=0; row<maze.getHeight(); row++) {
+                Cell cell = maze.getFinishedMaze()[col][row];
+
+
+                if (cell.isNorthWall()){
+                    g2d.drawLine(cell.getxPos()*multiplier+adjustor,(cell.getyPos()+1)*multiplier+adjustor,(cell.getxPos()+1)*multiplier+adjustor,(cell.getyPos()+1)*multiplier+adjustor);
+                }
+                if (cell.isSouthWall()){
+                    g2d.drawLine(cell.getxPos()*multiplier+adjustor,(cell.getyPos())*multiplier+adjustor,(cell.getxPos()+1)*multiplier+adjustor,(cell.getyPos())*multiplier+adjustor);
+                }
+                if (cell.isEastWall()) {
+                    g2d.drawLine((cell.getxPos()+1)*multiplier+adjustor,(cell.getyPos())*multiplier+adjustor,(cell.getxPos()+1)*multiplier+adjustor,(cell.getyPos()+1)*multiplier+adjustor );
+                }
+                if (cell.isWestWall()) {
+                    g2d.drawLine((cell.getxPos())*multiplier+adjustor,(cell.getyPos())*multiplier+adjustor,(cell.getxPos())*multiplier+adjustor,(cell.getyPos()+1)*multiplier+adjustor );
+                }
+            }
+        }
+    }
+
+    public int getWindowHeight() {
+        return windowHeight;
+    }
+
+    public int getWindowWidth() {
+        return windowWidth;
+    }
+}
